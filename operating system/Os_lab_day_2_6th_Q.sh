9a3e128f6226f91256f7a76d1344eeb8875d21e1
srivathsav999 <142774435+srivathsav999@users.noreply.github.com>
2024-05-06 18:19:28 +0530

    Update Os_lab_day_2_6th_Q.sh

--- operating system/Os_lab_day_2_6th_Q.sh
+++ operating system/Os_lab_day_2_6th_Q.sh
@@ -1,1 +1,22 @@
 
+# write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file. Also perform an ls command against the file or directory with the long listing option.
+# write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file. Also perform an ls command against the file or directory with the long listing option.
+#!/bin/bash
+
+echo "Enter the name of a file or directory:"
+read file_name
+
+if [ ! -e "$file_name" ]; then
+    echo "The file or directory '$file_name' does not exist."
+    exit 1
+fi
+
+if [ -d "$file_name" ]; then
+    echo "'$file_name' is a directory."
+elif [ -f "$file_name" ]; then
+    echo "'$file_name' is a regular file."
+else
+    echo "'$file_name' is another type of file."
+fi
+
+ls -l "$file_name"
