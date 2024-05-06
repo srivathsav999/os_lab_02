90872f55a37ea33a192a64950efe6eb12955f914
srivathsav999 <142774435+srivathsav999@users.noreply.github.com>
2024-05-06 18:10:17 +0530

    Update Os_lab_day_2_5th_Q.sh

--- operating system/Os_lab_day_2_5th_Q.sh
+++ operating system/Os_lab_day_2_5th_Q.sh
@@ -1,1 +1,2 @@
-
+#Write a shell script that displays “man”,”bear”,”pig”,”dog”,”cat”,and “sheep” on the screen with each appearing on a separate line. Try to do this in as few lines as possible.
+echo -e "man\nbear\npig\ndog\ncat\nsheep"
